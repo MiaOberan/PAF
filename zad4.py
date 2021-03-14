84923e8a99d4a24abc64cde11f3a5cde4b953e65
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-03-14 20:36:34 +0100

    cetvrti.

--- zad4.py
+++ zad4.py
@@ -3,9 +3,9 @@ def udaljenost_tocke(x1,y1,x2,y2,r):
     x=(x1-x2)**2+(y1-y2)**2
     d=sqrt(x)
     if d<r:
-        print(f"točka je unutar kružnice, a udaljenost je {d}")
+        print(f"točka se nalazi unutar kružnice, a udaljenost je {d}")
     elif d==r:
-        print(f"točka je na kružnici, a udaljenost je {d}")
+        print(f"točka se nalazi na kružnici, a udaljenost je {d}")
     else:
-        print(f"tocka je izvan kruznice, a udaljenost je {d}")
+        print(f"tocka se nalazi izvan kruznice, a udaljenost je {d}")
 udaljenost_tocke(2,3,1,4,5)
