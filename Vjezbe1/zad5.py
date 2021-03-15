fabb1f37b47e754c8aa25e03886cd78928c472dc
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-03-15 16:22:16 +0100

    Update zad5.py

--- Vjezbe1/zad5.py
+++ Vjezbe1/zad5.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 def odredi_pravac(x1,y1,x2,y2, znak):
     xkoordinate=[x1,x2]
     ykoordinate=[y1,y2]
-    plt.plot(xkoordinate,ykoordinate)
     if znak==0:
         plt.show()
     elif znak==1:
         naziv=input("Unesite naziv slike:")
         plt.savefig(f"{naziv}.pdf")
-odredi_pravac(2,4,5,1,0)
+    plt.plot(xkoordinate,ykoordinate, 'go--', linewidth=2, markersize=12)
+    plt.show()
+odredi_pravac(3,4,5,7,1)
