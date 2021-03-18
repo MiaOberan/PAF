c141627231a432d547590095f4bd0b5fc0a05ce6
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-03-18 22:35:05 +0100

    Update zad3.py

--- Vjezbe2/zad3.py
+++ Vjezbe2/zad3.py
@@ -1,7 +1,7 @@
 from kinematika import jednoliko_gibanje, kosi_hitac
-def gibanje(a):
+def graf(x):
   if x==a:
     jednoliko_gibanje(8,9,10)
-   elif x==2:
+   elif x==b:
     kosi_hitac(20,30,5)
-gibanje(1)
+graf(a)
