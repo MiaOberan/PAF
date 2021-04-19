00cc2979d79695d838bb7d0638f4094442bd8ce4
unknown <mia@oberan.com>
2021-04-19 13:41:55 +0200

    evo

---
+++ kolokvij/zad2.py
@@ -0,0 +1,6 @@
+import zad1 as z1
+p1=z1.VertikalniHitac(3,4)
+p1.objekt()  
+p2=z1.VertikalniHitac(2,6)
+p2.reset_visina()
+p2.objekt()
---
+++ kolokvij/zad3.py
@@ -0,0 +1,2 @@
+import zad1 as zd1
+p3=zd1.vert_hitac(10,10)
