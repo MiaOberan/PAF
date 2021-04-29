02dacf0e355985969782d6e841f1c7e66d694769
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-04-29 22:03:49 +0200

    Update zad3.cpp

--- vjezbe6/zad3.cpp
+++ vjezbe6/zad3.cpp
@@ -1,47 +1,44 @@
 #include <iostream>
-#include <math.h>
+#include <list>
+#include <iterator>
 
 using namespace std;
 
-const int n = 7;
-int lista[n] = {1,3,2,4,6};
+void polje(list <int> lista){
 
-void polje(unsigned a, unsigned b) {
-    for(int i = 0; i < n; i++) {
-        if (lista[i] >= a && lista[i] <= b) {
-            cout << lista[i] << " ";
-        }
-        else {
-            continue;
-        } } 
-        cout << endl;
-        }   
+    list <int> :: iterator it;
+    for(it = lista.begin(); it != lista.end(); ++it)
+        cout << '\t' << *it;
+    cout << '\n';
+    
+}
 
 int main() {
 
     list<int> lista;
     int a = 1;
-    int b = 7;
+    int b = 9;
     lista.push_back(a);
-    lista.push_back(2);
-    lista.push_back(4);
     lista.push_back(3);
-    lista.push_back(6);
+    lista.push_back(7);
+    lista.push_back(2);
+    lista.push_back(5);
     lista.push_back(b);
 
-    cout << "\nPrvotna lista je : ";
+    cout << "\nOriginalna lista: ";
     polje(lista);
 
-    cout << "\nLista poslije mijenjanjaa redoslijeda clanova: ";
+    cout << "\nLista nakon mijenjanja redoslijeda: ";
     lista.reverse();
     polje(lista);
 
-    cout << "\nLista nakon zamjene clanova: ";
+    cout << "\nLista nakon zamjene: ";
     std::swap(a, b);
     polje(lista);
 
-    cout << "\nLista nakon sortiranja: ";
+    cout << "\nLista nakon sortiranja po velicini : ";
     lista.sort();
     polje(lista);
 
     return 0;
+}
