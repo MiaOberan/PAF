2fa84e67e76f0c0b00cb253f7f1f49ba8c18def1
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-04-29 09:17:58 +0200

    Update module.py

--- Vjezbe4/module.py
+++ Vjezbe4/module.py
@@ -1,2 +1,8 @@
-def deriv(funkc,x,h):
-    return(funkc(x+h)-funkc(x-h))/(2*h)
+import calculus as ca
+def f1(x):
+    return 5*x**2-3*x
+def if1(x):
+    return 5*x**3/3 - 3*x**2/2
+a,b=ca.integracija1(f1,0.0,1.0,1000)
+print(a,b)
+print(if1(1)-if1(0))
