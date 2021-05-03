813b43eec9eb9b35a679ade401183e48673f90bb
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-05-03 13:36:12 +0200

    Update and rename grafovi.py to graf_deriv.py

--- Vjezbe4/graf_deriv.py
+++ Vjezbe4/graf_deriv.py
@@ -1,8 +1,9 @@
 import calculus
 import matplotlib.pyplot as plt
-import modul
+import module
 import math
 import numpy as np
+
 def f4(x):
     return 5*x**3 - 2*x**2+2*x-3
 xk1 = -2
@@ -11,15 +12,20 @@ h = 0.1
 x_l1 = np.arange(xk1,xk2,h)
 d_l1 = []
 for x in x_l1:
-    d = 15*x**2 - 4*x +2
+    d =15*x**2 - 4*x +2
     d_l1.append(d)
 
-a,b = calculus.derivacija(f4,0.1,-3,3)
+a,b = calculus.deriviraj(f4,0.1,-2,2)
 plt.plot(x_l1,d_l1)
 plt.scatter(a,b, s = 5, color = 'm')
 plt.show()
 
-def ftrig(x):
+print(calculus.deriv2(f4,1,0.1))
+x=1
+d =15*x**2 - 4*x +2
+print(d)
+
+def ftrigon(x):
     return math.sin(2*x) - math.cos(x)
 xt1 = -10
 xt2 = 10
@@ -31,7 +37,7 @@ for x in x_l2:
     d = math.cos(2*x)*2+math.sin(x)
     d_l2.append(d)
 
-c,d = calculus.derivacija(ftrig,0.01,-10,10)
+c,d = calculus.deriviraj(ftrigon,0.01,-10,10)
 plt.plot(x_l2,d_l2)
 plt.scatter(c,d, s = 1, color = 'k')
 plt.show()
