b5a3b7f1b8782665c1a9a68f44d13f1392a2085c
unknown <mia@oberan.com>
2021-06-10 08:39:09 +0200

    vj 11

---
+++ vj11/zad1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import math
+class gravity:
+    def __init__(self,ms,mz,G,xs,ys,xz,yz,vxs,vys,vxz,vyz,dt):
+        self.ms = ms
+        self.mz = mz
+        self.G = G
+        self.rs = np.array([xs,ys])
+        self.rz = np.array([xz,yz])
+        self.vs = np.array([vxs,vys])
+        self.vz = np.array([vxz,vyz])
+        self.dt = dt
+        self.t = 0
+        self.xs_ = []
+        self.ys_ = []
+        self.xz_ = []
+        self.yz_ = []
+    def akcel_s(self,rs,rz):
+        d = (rs-rz)**2
+        n = math.sqrt(d[0]+d[1])
+        return (-self.G*self.mz*(rs-rz))/n**3
+    def akcel_z(self,rs,rz):
+        d = (rz-rs)**2
+        n = math.sqrt(d[0]+d[1])
+        return (-self.G*self.ms*(rz-rs))/n**3
+    def Euler(self,t):
+        while self.t <= t:
+            self.asn = self.akcel_s(self.rs,self.rz)
+            self.vs = self.vs + self.asn*self.dt
+            self.rs = self.rs + self.vs*self.dt
+            
+            self.az = self.akcel_z(self.rs,self.rz)
+            self.vz = self.vz + self.az*self.dt
+            self.rz = self.rz + self.vz*self.dt
+            
+            self.t+= self.dt
+            self.xs_.append(self.rs[0])
+            self.ys_.append(self.rs[1])
+            self.xz_.append(self.rz[0])
+            self.yz_.append(self.rz[1])
+            
+        return self.xs_,self.ys_,self.xz_,self.yz_
+
