9f37648474cada70fae1b06f0de5c0dfadf1626f
MiaOberan <80029163+MiaOberan@users.noreply.github.com>
2021-06-21 21:52:46 +0200

    Create 24.py

---
+++ practising/24.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import math
+from kosi_hitac import crtaj_x_y_graf
+from kosi_hitac import izracunaj_maks_visinu
+from kosi_hitac import izracunaj_domet
+from kosi_hitac import izracunaj_maks_brzinu
+from kosi_hitac import provjeri_metu
+
+crtaj_x_y_graf(50,30,25)
+izracunaj_maks_visinu(50,30,25)
+izracunaj_domet(50,30,25)
+izracunaj_maks_brzinu(50,30,25)
+provjeri_metu(50,30,25,150,35,0.5)
